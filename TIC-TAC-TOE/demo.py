e98633c9a2fb86fe6461898365931c60bf331fdb
Rajat Singh <destroyer@mahadevbhakt.fire>
2021-07-15 02:43:31 +0530

    changes to TIC-TAC-TOE

--- TIC-TAC-TOE/demo.py
+++
@@ -1,8 +0,0 @@
-# demo = [' ' for _ in range(9)]
-# print(demo)
-# for row in [demo[i * 3: (i + 1) * 3] for i in range(3)]:
-#     print('| ' + ' | '.join(row) + ' |')
-
-# number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
-# for row in number_board:
-#     print('| ' + ' | '.join(row) + ' |')
